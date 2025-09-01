478044b930a2e06ea4bdaac15d575b88a4c10e10
Rishika-Jetlearn <vreakiwala.vk@gmail.com>
2025-09-01 16:55:17 +0100

    update

--- padas data frame.py
+++ padas data frame.py
@@ -12,7 +12,43 @@ print(d_dataframe)
 print(d_dataframe.index)
 print(d_dataframe.columns)
 print(data_in_frame.index)
+
 #fetch complete info
 d_dataframe.info()
+
 #descriptive statistics
 print(d_dataframe.describe())
+
+
+#reading csv file
+titanic_df=pd.read_csv(r"C:\Users\Manisha\Desktop\Jetlearn-python-rishika\Data-Science\titanic.csv")
+print(titanic_df)
+print(titanic_df.info())
+
+#fetch records from top,it is 5
+print(titanic_df.head(3))
+#fetch records from bottom
+print(titanic_df.tail())
+
+#finding shape,number of rows and coloumns
+print(titanic_df.shape)
+
+#retriving singe coloumn values(series)
+print(titanic_df["Age"])
+print(titanic_df["Age"].max())
+print(titanic_df["Fare"].sum())
+
+#fetching multiple coloumns
+print(titanic_df[["Name","Age"]])
+
+#filitering(selecting by condititon)
+print(titanic_df[titanic_df["Age"]<18])
+print(titanic_df[(titanic_df["Age"]<18)&(titanic_df["Pclass"]==1)])# or is done with |
+
+#slicing with index
+print(titanic_df.iloc[3:15:2,2:4])
+print(titanic_df.iloc[[3,4,6],[2,5,6]])
+
+#conditional slicing
+print(titanic_df.loc[titanic_df["Age"]<18,["Name","Fare"]])
+print(titanic_df.loc[1:5,["Name","Age"]])
